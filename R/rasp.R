b746f82d542193e1f990ccc3f0f9d2362a9d66ba
Pol Cuscó <polcusco@gmail.com>
2020-03-30 15:39:27 +0200

    Now using dgarrimar/mlm package to compute p-values.

--- R/rasp.R
+++ R/rasp.R
@@ -1,5 +1,6 @@
 rasp <- function(x, y, expressionCols, geneidCol, test = c("asymptotic", "permutation"), filter = 0.1, filterFreq = 0.05, transformation = FALSE, Nperms = 1e5-1, type = "median", pairwise = FALSE, mc.cores = 1, maxTrans = 100, testGroup = FALSE, verbose = FALSE) {
-    filt <- TRUE # TODO: what does this undefined variable do?
+    # TODO: add support for formula notation.
+    if (0) next
     
     # TODO: Remove support for ExonCountSet, since is deprecated.
     if (class(x) == "ExonCountSet") {
@@ -9,7 +10,7 @@ rasp <- function(x, y, expressionCols, geneidCol, test = c("asymptotic", "permut
         } else if (!is.factor(y)) stop("'y' should be a factor") 
 
         x2 <- x
-        xx <- split(x, droplevels(as.factor(geneIDs(x2)[filt])))
+        xx <- split(x, droplevels(as.factor(geneIDs(x2))))
     
     } else if (class(x) == "DEXSeqDataSet") {
         if(missing(y)) {
@@ -18,7 +19,7 @@ rasp <- function(x, y, expressionCols, geneidCol, test = c("asymptotic", "permut
         } else if (!is.factor(y)) stop("'y' should be a factor")
         
         x2 <- x
-        xx <- split(x, droplevels(as.factor(geneIDs(x2)[filt])))
+        xx <- split(x, droplevels(as.factor(geneIDs(x2))))
         
     } else if (class(x) == "RangedSummarizedExperiment") {
       # if(missing(y)) {
@@ -28,69 +29,41 @@ rasp <- function(x, y, expressionCols, geneidCol, test = c("asymptotic", "permut
       if (!is.factor(y)) stop("'y' should be a factor")
       
       x2 <- x
-      xx <- lapply(split(x, names(rowRanges(x))[filt]), assay)
+      message('Loading data...')
+      xx <- lapply(split(x, names(SummarizedExperiment::rowRanges(x))), SummarizedExperiment::assay)
       
     } else {
         if (!is.factor(y)) stop("'y' should be a factor") 
         xx <- split(x[, expressionCols], droplevels(as.factor(x[, geneidCol])))
     }
     
-    test <- match.arg(test)
-    if (test == "asymptotic") {
-        # TODO: are these ever used?
-        n <- ncol(x)
-        g <- nlevels(y)
-    }
-
-    # assign("aux", 0, .GlobalEnv)
+    # Compute over all genes.
     pb <- txtProgressBar(max = length(xx), style = 3)
-    masterDesc <- try(get("masterDescriptor", envir = getNamespace("parallel")), TRUE)
     
-    # TODO: take these functions outside.
-    f <- function(x, group, test, Nperms, nc, coreID, type){
-        masterDesc <- get("masterDescriptor", envir = getNamespace("parallel"))
-        if (masterDesc() == coreID) {
-            # auxx <- get("aux", envir = .GlobalEnv)
-            assign("aux", auxx + 1, .GlobalEnv)
-            setTxtProgressBar(pb, nc * aux)
-        }
-        testRasp(x = x, group = group, test = test, type = type, Nperms = Nperms,
-                 transformation=transformation, multipleTesting=TRUE, maxTrans=maxTrans,
-                 pairwise = pairwise, testGroup = testGroup, filter = filter)
-    }
-    f2 <- function(nm, group, test, type, Nperms, transformation, verb){
-        if (verb) print(nm)
-        # aux <<- aux + 1
-        # setTxtProgressBar(pb, aux)
-        setTxtProgressBar(pb, getTxtProgressBar(pb) + 1)
-        testRasp(x = xx[[nm]], group = group, test = test, type = type, Nperms = Nperms,
-                 transformation=transformation, multipleTesting=TRUE, maxTrans=maxTrans,
-                 pairwise = pairwise, testGroup = testGroup, filter = filter)
-    }
-    
-    if (mc.cores > 1) {
-        if (class(masterDesc) == "try-error")
-            stop("It appears you are trying to use multiple cores from Windows, this is not possible")
-        mclapp <- try(get("mclapply", envir = getNamespace("parallel")), TRUE)
-        detectCor <- try(get("detectCores", envir = getNamespace("parallel")),TRUE)
-        nAvailableCores <- detectCor()
-        coreID <- mclapp(as.list(1:mc.cores), function(x) masterDesc(),
-                         mc.cores = mc.cores)[[1]]
-        ans <- mclapp(xx, f, group=y, test=test, Nperms=Nperms,
-                      nc = mc.cores, coreID = coreID, type = type, mc.cores = mc.cores)
-    } else {
-        ans <- lapply(names(xx), f2, group=y, test=test, type = type, Nperms=Nperms,
-                      transformation=transformation, verb = verbose)
-    }
+    ans <- list()
+    # doParallel::registerDoParallel(cores = parallel::detectCores())
+    # ans <- foreach::`%dopar%`(foreach::foreach (nm = 1:length(xx)), {
+    for (nm in 1:length(xx)) {
+      if (all(xx[[nm]] == 0)) ans[[nm]] <- NA # mlm crashes on empty (0 count) and single-exon genes.
+      else if (nrow(xx[[nm]]) == 1) ans[[nm]] <- NA # mlm crashes on single exon genes.
+      else ans[[nm]] <- mlm::mlm(t(xx[[nm]]) ~ y)$aov.tab["y", "Pr(>F)"]
+      
+      setTxtProgressBar(pb, getTxtProgressBar(pb) + 1)
+    }#)
+
     setTxtProgressBar(pb, length(xx))
     
-    out <- do.call(rbind, ans) 
-    nlev <- nlevels(y)
-    pvals <- out[, -c(1:(nlev + 1)), drop = FALSE]
+    # Post-process results.
+    out <- do.call(rbind, ans) # TODO: substitute by unlist.
+    # nlev <- nlevels(y)
+    # pvals <- out[, -c(1:(nlev + 1)), drop = FALSE]
+    pvals <- out[, 1, drop = FALSE] # TODO: remove this, just call pvals the previous 'out' variable.
     if(ncol(pvals) > 1) pvals[, 2:ncol(pvals)] <- t(apply(pvals[,2:ncol(pvals)], 1, p.adjust, method = "holm"))
     pvals.adj <- apply(pvals, 2, p.adjust, "BH")
-    colnames(pvals.adj) <- gsub("pvalue", "padjust", colnames(pvals.adj))
+    # colnames(pvals.adj) <- gsub("pvalue", "padjust", colnames(pvals.adj))
     out <- as.data.frame(cbind(out, pvals.adj))
+    names(out) <- c("pvalue", "padjust")
+    rownames(out) <- names(xx)
     class(out) <- c("data.frame", "rasp")
     
     close(pb)
