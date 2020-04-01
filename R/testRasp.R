7b4ea60ed34bde34e7cc84572383c5dca1063630
Pol Cuscó <polcusco@gmail.com>
2020-04-01 13:21:51 +0200

    Added progress bar. Fixed bug when filtering exons.

--- R/testRasp.R
+++ R/testRasp.R
@@ -1,39 +1,44 @@
 testRasp <- function(x, data, filterInd, filterExon, transform, ...) {
-    
-    if(nrow(x)!=length(data$group))
+    # Check arguments.
+    if (filterInd < 0 | filterInd > 1) stop("'filterInd' must be between 0 and 1")
+    if (filterExon < 0 | filterExon > 1) stop("'filterExon' must be between 0 and 1")
+  
+    if (nrow(x) != length(data$group))
         stop("number of individuals doesn't match")
-    if(any(is.na(data$group)))
+    if (any(is.na(data$group)))
         stop("'group' variable contains missing values")
-    if(filterInd > 0){
-        m <- apply(x > 0, 1, mean)
+  
+    # Filter individuals (samples).
+    if (filterInd) {
+        m <- rowMeans(x > 0)
         nfilt <- sum(m < filterInd, na.rm = TRUE)
-        if(nfilt > 0){
+        if (nfilt) {
             ind.keep <- m >= filterInd
-            x <- x[ind.keep,]
+            x <- x[ind.keep, , drop = FALSE]
             warning(nfilt, " individuals filtered out given 'filterInd' argument")
+            # TODO: if all individuals excluded, return NA.
         }
-        else{
-            ind.keep <- rep(TRUE, nrow(x))
-        }
+        else ind.keep <- TRUE
     }
-    if(filterExon > 0){
+  
+    # Filter exons.
+    if (filterExon) {
         ## Filter exons with mean relative frequency < 'filterFreq'
         auxa1 <- rowSums(x)
         auxR <- sweep(x, 1, auxa1, FUN="/")
         mrf <- colMeans(auxR)
         nffilt <- sum(mrf < filterExon, na.rm = TRUE)
         if(nffilt > 0){
-            x <- x[, mrf >= filter]
+            x <- x[, mrf >= filterExon]
             warning(nffilt, " exons filtered out given 'filterExon' argument")
+            # TODO: if all exons excluded, return NA.
         }
     }
     
     data <- data[ind.keep, , drop=FALSE]
     
+    # Compute p-values.
     mod <- try(mlm::mlm(x ~ ., data=data, transform=transform, ...), TRUE)
-    if (!inherits(mod, "try-error"))
-        out <- mod$aov.tab["group", "Pr(>F)"]
-    else
-        out <- NA
-    return(out)
+    if (!inherits(mod, "try-error")) mod$aov.tab["group", "Pr(>F)"]
+    else NA
 }
